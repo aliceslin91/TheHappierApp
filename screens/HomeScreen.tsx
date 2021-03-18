0f8bc4738bcf65214f0a8f3b9fa14a1c66a5ce49
Alice Lin <aliceslin91@gmail.com>
2021-03-18 12:13:19 -0700

    Add build number onto home screen

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -2,11 +2,16 @@ import React from "react";
 import { Text, View } from "react-native";
 
 import styles from "../constants/BaseStyle";
+import appSettings from "../app.json";
 
 export default function HomeScreen() {
   return (
     <View style={[styles.container, styles.centered]}>
       <Text style={styles.header}>Placeholder screen</Text>
+      <Text style={styles.paragraph}>
+        Version {appSettings.expo.version} build{" "}
+        {appSettings.expo.ios.buildNumber}
+      </Text>
     </View>
   );
 }
